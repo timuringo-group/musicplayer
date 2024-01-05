fd6030a22ee62e2af2c0ccbfde0146c31fad01a5
timu-119 <98218719+tmyt105@users.noreply.github.com>
2024-01-05 11:52:05 +0900

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,27 @@
+
+var CACHE_NAME = 'pwa-tmyt105music-caches';
+var urlsToCache = [
+    '/tmyt105-musicplayer',
+];
+
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
