85f2273f5a4ecd96d9bfe701f8954ef4f704084b
timu-119 <98218719+tmyt105@users.noreply.github.com>
2024-01-13 21:17:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let song = document.getElementById('song');
 let loadFileButton = document.getElementById('loadFileButton');
 let fileInput = document.getElementById('fileInput');
 let loopButton = document.getElementById('loopButton');
-let dropArea = document.getElementById('dropArea');
 
 loadFileButton.addEventListener('click', function() {
  fileInput.click();
@@ -16,16 +15,16 @@ function handleFiles(files) {
  song.src = url;
 }
 
-dropArea.addEventListener('drop', function(event) {
+window.addEventListener('dragover', function(event) {
+ event.preventDefault();
+}, false);
+
+window.addEventListener('drop', function(event) {
  event.preventDefault();
  handleFiles(event.dataTransfer.files);
-});
+}, false);
 
 loopButton.addEventListener('click', function() {
  song.loop = !song.loop;
  loopButton.textContent = song.loop ? 'ループ解除' : 'ループ';
 });
-
-function showDropIndicator(isShow) {
- dropArea.style.backgroundColor = isShow ? '#ddd' : '';
-}
